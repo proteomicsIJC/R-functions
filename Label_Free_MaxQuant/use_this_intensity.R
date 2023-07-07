4f69f2a39dc197e6648049da39d7ab0064e1cb2e
Iggi-29 <ignasijs29@gmail.com>
2023-07-07 10:01:35 +0200

    added use this intensity for label free

---
+++ Label_Free_MaxQuant/use_this_intensity.R
@@ -0,0 +1,6 @@
+use_this_intensity <- function(dataset,pattern){
+  intensities <- grep(pattern = pattern, colnames(dataset))
+  major <- grep(pattern = "Majority protein IDs", colnames(dataset))
+  dataset <- dataset[,c(c(major),
+                        c(intensities))]
+}
