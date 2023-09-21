2167e50f40670d6e209d56fc30b9b87f793b62dd
Iggi-29 <ignasijs29@gmail.com>
2023-09-21 14:33:21 +0200

    updated functions to be lazy when looking for pattern

--- general/log2_to_pattern.R
+++ general/log2_to_pattern.R
@@ -5,7 +5,7 @@ log2_to_pattern <- function(patterns,dataset){
   cat(paste0("Intensity values have been log2 transformed and negative values after transformation have been assigned to NA"), file = "./results/used_parameters.txt", sep = "\n", append = T)
   cat(paste0(rep("_",50), collapse = ""), file = "./results/used_parameters.txt",append = T, sep = "\n")
   for (i in 1:length(patterns)){
-    searching <- grep(pattern = paste0("^",patterns[[i]],"$"), names(dataset))
+    searching <- grep(pattern = patterns[[i]], names(dataset))
     dataset[searching] <- log2(dataset[searching])
     dataset[searching][dataset[searching] <= 0] <- NA}
   return(dataset)
--- general/zero_to_na.R
+++ general/zero_to_na.R
@@ -1,7 +1,7 @@
 zero_to_NA <- function(patterns,dataset) {
   print("Zero values have been transformed to NAs")
   for (i in 1:length(patterns)){
-    searching <- grep(pattern = paste0("^",patterns[[i]],"$"), names(dataset))
+    searching <- grep(pattern = patterns[[i]], names(dataset))
     dataset[searching][dataset[searching] == 0] <- NA}
   return(dataset)
 }
