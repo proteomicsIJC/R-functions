5788d4dc1fce0fa3ddaa070c08a72283184e2bea
Iggi-29 <ignasijs29@gmail.com>
2023-10-16 09:51:05 +0200

    Making sure i can make changes

--- Label_Free_MaxQuant/use_this_intensity.R
+++ Label_Free_MaxQuant/use_this_intensity.R
@@ -2,5 +2,5 @@ use_this_intensity <- function(dataset,pattern){
   intensities <- grep(pattern = pattern, colnames(dataset))
   major <- grep(pattern = "Majority protein IDs", colnames(dataset))
   dataset <- dataset[,c(c(major),
-                        c(intensities))]
+                        ####c(intensities))]
 }
