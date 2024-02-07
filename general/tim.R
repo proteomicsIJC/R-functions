889e67a768285410404b91f759d2b148a5149a06
Iggi-29 <ignasijs29@gmail.com>
2024-02-07 09:56:51 +0100

    better instructions

--- general/tim.R
+++ general/tim.R
@@ -3,11 +3,12 @@
 #########
 
 # impute: yes or no, whether if we want to impute the data or not
-# presence_no_presence: to get all the data with all the missing values
-# report results: in case we have to save the results or not
-# intensity_to_impute: colname of the intensity we want to apply the imputation on
 # dataset: a long format dataframe: it requires to have a intensity column, a proteim_group column and a exp_group column that will define the NAs_prpp proportion
+## and also a sample_name column
 # NAs_prop: the max proportion of NA values per group to consider the nimber to enter the imputation
+# presence_no_presence: to get all the data with all the missing values
+# intensity_to_impute: colname of the intensity we want to apply the imputation on
+# report results: in case we have to save the results or not
 
 
 tim <- function(impute,dataset,NAs_prop,presence_no_presence = F, report_results = T, intensity_to_impute = ""){
