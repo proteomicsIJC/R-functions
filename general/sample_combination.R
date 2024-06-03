e69317b12b9383d8f4de098ae641a7e9dd200002
Iggi-29 <ignasijs29@gmail.com>
2024-06-03 12:04:00 +0200

    Sample combination function is now operative and works fine

--- general/sample_combination.R
+++ general/sample_combination.R
@@ -5,15 +5,16 @@
 ## dataset = a long format dataframe with some intensity to combine between technical reps
 ## technical_replicates = column for the technical reps
 ## sample_names = column for the real sample names
+## expression_unit = the grouping column for the entries of each sample (Protein.Group,Accession,Gene.names ...)
 ## intensity_to_combine = column with the intensity to combine
 ## other_intensities = column with the names of other intensities they will be removed if remove_reps is T
 ## remove_reps = remove technical replicates column and also remove the other_intensities cols
 ## report results = whether if the process of the function should be reported or not in the used_parameters file
 
 sample_combination <- function(dataset,
-                               technical_replicates = "", sample_names = "", 
-                               intensity_to_combine = "", remove_reps = F,
-                               other_intensities = NULL,
+                               technical_replicates = "", sample_names = "",
+                               expression_unit = "", intensity_to_combine = "",
+                               remove_reps = F, other_intensities = NULL,
                                report_results = T){
   if (report_results){
   cat("sample_combination",file = "./results/used_parameters.txt",sep = "\n", append = T)
@@ -22,18 +23,22 @@ sample_combination <- function(dataset,
   
   if (isTRUE(remove_reps)){
     dataset <- dataset %>% 
-      group_by(get(sample_names)) %>% 
+      group_by(get(sample_names),
+               get(expression_unit)) %>% 
       mutate(combined_intensity = median(get(intensity_to_combine), na.rm = T)) %>% 
       subset(select = -c(get(technical_replicates), get(intensity_to_combine))) %>% 
       distinct()
     if (!is.null(other_intensities)){
       dataset <- dataset %>% 
-        subset(select = -c(other_intensities))}} 
+        subset(select = -c(get(other_intensities))) %>% 
+        distinct()}} 
   if (isFALSE(remove_reps)){
     dataset <- dataset %>% 
-      group_by(get(sample_names)) %>% 
+      group_by(get(sample_names),
+               get(expression_unit)) %>% 
       mutate(combined_intensity = median(get(intensity_to_combine), na.rm = T))
   }
+  dataset <- dataset[,!grepl("^get\\(",colnames(dataset))]
   return(dataset)
 }
 
