e8d1cd4af288150b6bd622a9c5112913070a0aa4
Iggi-29 <ignasijs29@gmail.com>
2024-07-15 16:41:26 +0200

    column checker and tim do not require extra columns

--- general/columns_checker.R
+++ general/columns_checker.R
@@ -1,18 +1,24 @@
-#######################
-### columns_checker ###
-#######################
-
-## dataset = A long format dataframe with a column named sample_name and a second column named normalized_intensity  
-
-columns_checker <- function(dataset){
+#' Create a contrast matrix with all possible contrasts given a design matrix 
+#' @param dataset a long format dataset with a column named sample_name
+#' @param unit_of_analysis the unit of the analysis
+#' @param intensity the column name of the intensity information that is going to be use
+#' @param tmt TRUE of FALSE, if TRUE, it is expected that the analysis is done on TMT samples and expects a column named
+#' sample_or_pool, where "sample" indicates a sample and POOL indicates that that sample is really a POOL
+#' @export
+columns_checker <- function(dataset,
+                            unit_of_analysis = "Protein.Group",
+                            intensity = "normalized_intensity",
+                            tmt = F){
   print("Note that only those groups that will be printed will be those corresponding to samples and not pools")
-  batching <- dataset %>% 
-    filter(sample_or_pool == "sample")
+  if (isFALSE(tmt)){
+  batching <- dataset}
+  if (isTRUE(tmt)){
+    batching <- dataset %>% 
+    filter(sample_or_pool == "sample")}
   
   batching <- reshape2::dcast(batching, 
-                    protein_group ~ sample_name, value.var="normalized_intensity",
+                    get(unit_of_analysis) ~ sample_name, value.var = intensity,
                     fun.aggregate = mean)
   batching <- batching[,-1]
-   ###### HOLA !!!! SEGONA PROVA
   print(colnames(batching))
 }
--- general/tim.R
+++ general/tim.R
@@ -10,10 +10,29 @@
 #' @param intensity_to_impute  colname of the intensity we want to apply the imputation on
 #' @param report_results  in case we have to save the results or not
 #' @export
-tim <- function(impute,dataset,NAs_prop,presence_no_presence = F, report_results = T, intensity_to_impute = ""){
+tim <- function(impute,dataset,NAs_prop,presence_no_presence = F, report_results = T, intensity_to_impute = "", 
+                unit_to_impute = "Protein.Group", experimental_groups = NULL){
+  # meta_data for the imputation
+  # if it is NULL. all samples come from the same group
+  if(is.null(experimental_groups)){
+    meta_data_for_imputation <- as.data.frame(tibble::tibble(
+      sample_name = unique(dataset$sample_name),
+      exp_group = rep("Ungrouped", times = length(unique(dataset$sample_name)))
+      ))
+    dataset <- merge(dataset, meta_data_for_imputation, by = "sample_name")
+  }
+  
+  if(!is.null(experimental_groups)){
+    meta_data_for_imputation <- as.data.frame(tibble::tibble(
+      sample_name = names(experimental_groups),
+      exp_group = experimental_groups
+      ))
+    dataset <- merge(dataset, meta_data_for_imputation, by = "sample_name")
+  }
+  
   # PGs that have some NA
   for_nas <- dataset
-  cols <- c("protein_group")
+  cols <- c(unit_to_impute)
   for_nas <- for_nas %>%
     group_by(across(all_of(cols))) %>%
     mutate(count_NA = sum(is.na(get(intensity_to_impute)))) %>%
@@ -27,7 +46,7 @@ tim <- function(impute,dataset,NAs_prop,presence_no_presence = F, report_results
   n <- length(unique(dataset$sample_name))
   if (impute == "no"){
     print("No missing values imputation is performed, all PGs with more than 0 NA values will be removed")
-    cols <- c("protein_group")
+    cols <- c(unit_to_impute)
     dataset <- dataset %>%
       group_by(across(all_of(cols))) %>%
       mutate(count_NA = sum(is.na(get(intensity_to_impute)))) %>%
@@ -51,7 +70,7 @@ tim <- function(impute,dataset,NAs_prop,presence_no_presence = F, report_results
   if (impute == "yes"){
     print("Missing values will be imputed using mice. A maximum proportion of ")
     print(paste0("A maximum proportion of ",NAs_prop," is accepted"))
-    cols <- c("protein_group","exp_group")
+    cols <- c(unit_to_impute,"exp_group")
     dataset <- dataset %>%
       group_by(across(all_of(cols))) %>%
       mutate(count_NA = sum(is.na(get(intensity_to_impute)))) %>%
@@ -65,9 +84,9 @@ tim <- function(impute,dataset,NAs_prop,presence_no_presence = F, report_results
     
     # Reshape the data to impute
     to_imput <- reshape2::dcast(dataset, 
-                                protein_group ~ sample_name, value.var= intensity_to_impute, fun.aggregate = median)
+                                get(unit_to_impute) ~ sample_name, value.var= intensity_to_impute, fun.aggregate = median)
     
-    rownames(to_imput) <- to_imput$protein_group
+    rownames(to_imput) <- to_imput[,1]
     to_imput <- to_imput[,-1]
     
     # Impute the data
@@ -79,17 +98,17 @@ tim <- function(impute,dataset,NAs_prop,presence_no_presence = F, report_results
     values <- unlist(as.vector(imputed_data), use.names = F)
     
     imputed_data_long <- data_frame("sample_name" = samples,
-                                    "protein_group" = pgs,
+                                    unit_to_impute = pgs,
                                     imputed_intensity = values)
-    
+    colnames(imputed_data_long)[2] <- unit_to_impute
     #####dataset[,"normalized_intensity_before_imputation"] <- dataset[,intensity_to_impute]
     #####    dataset <- subset(dataset, select = -c(normalized_intensity))
     
-    imputed_data <- merge(imputed_data_long, dataset, by = c("sample_name","protein_group"))
+    imputed_data <- merge(imputed_data_long, dataset, by = c("sample_name",unit_to_impute))
     imputed_data <- imputed_data %>%
       relocate(imputed_intensity, .after = all_of(intensity_to_impute))
     
-    cols <- c("protein_group")
+    cols <- c(unit_to_impute)
     imputed_data <- imputed_data %>%
       group_by(across(all_of(cols))) %>%
       filter(n() == n) %>%
@@ -102,9 +121,9 @@ tim <- function(impute,dataset,NAs_prop,presence_no_presence = F, report_results
   if (presence_no_presence == T){
     cat("Returning an expression matrix with all NAs to do presence/No presence analysis")
     to_imput <- reshape2::dcast(dataset, 
-                                protein_group ~ sample_name, value.var= intensity_to_impute, fun.aggregate = median)
+                                get(unit_to_impute) ~ sample_name, value.var= intensity_to_impute, fun.aggregate = median)
     
-    rownames(to_imput) <- to_imput$protein_group
+    rownames(to_imput) <- to_imput[,1]
     to_imput <- to_imput[,-1]
     expression_mat_presence_no_presence <<- to_imput
     imputed_data <- "no data to be displayed as no imputation is done and this procedure is only done in order to not apply any statistics"
