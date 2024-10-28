eb4cff1063c84334aae4a0ddb89a947844d614b8
Iggi-29 <ignasijs29@gmail.com>
2024-10-28 17:34:08 +0100

    Better code organization for imputation

--- general/tim.R
+++ general/tim.R
@@ -15,122 +15,84 @@ tim <- function(impute,dataset,
                 intensity_to_impute = "", unit_to_impute = "Protein.Group",
                 presence_no_presence = F,  
                 report_results = T){
+  
+  ### Report results
+  if (report_results){
+    if (impute == "yes"){
+      cat("tim - for NA imputation",file = "./results/used_parameters.txt",sep = "\n", append = T)
+      cat(paste0("impute == yes ",NAs_prop), file = "./results/used_parameters.txt", sep = "\n", append = T)
+      cat(paste0(rep("_",50), collapse = ""), file = "./results/used_parameters.txt",append = T, sep = "\n")}
+    else if (impute == "no"){
+      cat("tim - for NA imputation",file = "./results/used_parameters.txt",sep = "\n", append = T)
+      cat(paste0("impute == no"), file = "./results/used_parameters.txt", sep = "\n", append = T)
+      cat(paste0(rep("_",50), collapse = ""), file = "./results/used_parameters.txt",append = T, sep = "\n")}
+  }
+  
   # meta_data for the imputation
   # if it is NULL. all samples come from the same group
   if(is.null(experimental_groups)){
-    meta_data_for_imputation <- as.data.frame(tibble::tibble(
+    meta_data_for_imputation <- tibble::tibble(
       sample_name = unique(dataset$sample_name),
       exp_group = rep("Ungrouped", times = length(unique(dataset$sample_name)))
-      ))
+    )
     dataset <- merge(dataset, meta_data_for_imputation, by = "sample_name")
   }
   
   if(!is.null(experimental_groups)){
-    meta_data_for_imputation <- as.data.frame(tibble::tibble(
-      sample_name = names(experimental_groups),
-      exp_group = experimental_groups
-      ))
+    meta_data_for_imputation <- dataset %>% 
+      subset(select = c(sample_name, get(experimental_groups))) %>% 
+      distinct()
+    colnames(meta_data_for_imputation)[2] <- "exp_group"
     dataset <- merge(dataset, meta_data_for_imputation, by = "sample_name")
   }
   
-  # PGs that have some NA
-  for_nas <- dataset
-  cols <- c(unit_to_impute)
-  for_nas <- for_nas %>%
-    group_by(across(all_of(cols))) %>%
-    mutate(count_NA = sum(is.na(get(intensity_to_impute)))) %>%
-    ungroup()
-  for_nas <- for_nas %>%
-    filter(count_NA > 0)
-  pg_with_nas <<- for_nas
-  
-  # Only NAs values
-  only_nas <<- dataset[!complete.cases(dataset),]
-  n <- length(unique(dataset$sample_name))
+  ### NO Nas (impute == "no")
+  # Remove all values with more than 0 NAs
   if (impute == "no"){
-    print("No missing values imputation is performed, all PGs with more than 0 NA values will be removed")
-    cols <- c(unit_to_impute)
     dataset <- dataset %>%
-      group_by(across(all_of(cols))) %>%
-      mutate(count_NA = sum(is.na(get(intensity_to_impute)))) %>%
-      ungroup()
-    
-    # Filter pg with less than NAs_prop
-    dataset <- dataset %>%
-      filter(count_NA == 0)
-    
-    # Filter pg with less than NAs_prop
-    dataset <- dataset %>% 
-      group_by(across(all_of(cols))) %>%
-      filter(n() >= length(unique(sample_name))) %>%
-      ungroup()
-    imputed_data <- dataset
-    if (report_results){
-    cat("tim - for NA imputation",file = "./results/used_parameters.txt",sep = "\n", append = T)
-    cat(paste0("impute == no"), file = "./results/used_parameters.txt", sep = "\n", append = T)
-    cat(paste0(rep("_",50), collapse = ""), file = "./results/used_parameters.txt",append = T, sep = "\n")}
+      group_by(get(unit_to_impute)) %>% 
+      mutate(number_of_NAs = sum(is.na(get(intensity_to_impute)))) %>% 
+      ungroup() %>% 
+      filter(number_of_NAs == 0) %>% 
+      subset(select = -c(number_of_NAs, `get(unit_to_impute)`)) 
   }
+  
+  ### Some NAs (impute == "yes")
   if (impute == "yes"){
-    print("Missing values will be imputed using mice. A maximum proportion of ")
-    print(paste0("A maximum proportion of ",NAs_prop," is accepted"))
-    cols <- c(unit_to_impute,"exp_group")
-    dataset <- dataset %>%
-      group_by(across(all_of(cols))) %>%
-      mutate(count_NA = sum(is.na(get(intensity_to_impute)))) %>%
-      mutate(per_NA = sum(is.na(get(intensity_to_impute))) /sum(sum(is.na(get(intensity_to_impute))) + sum(!is.na(get(intensity_to_impute))))) %>%
-      ungroup() %>%
-      mutate(was_NA = ifelse(is.na(get(intensity_to_impute)) == T,"Was NA","Was not NA"))
-    
-    # Filter pg with less than NAs_prop
-    dataset <- dataset %>%
-      filter(per_NA <= NAs_prop)
-    
+    n_of_samples <- length(meta_data_for_imputation$sample_name)
+    # Remove values with a percentage of NAs bigger than NAs_prop
+    dataset <- dataset %>% 
+      group_by(get(unit_to_impute), exp_group) %>% 
+      mutate(possible_values = n()) %>% 
+      mutate(number_of_NAs = sum(is.na(get(intensity_to_impute)))) %>%
+      mutate(per_NA = number_of_NAs/possible_values) %>% 
+      ungroup() %>% 
+      filter(per_NA <= NAs_prop) %>% 
+      subset(select = -c(`get(unit_to_impute)`, possible_values)) %>%
+      group_by(get(unit_to_impute)) %>% 
+      mutate(number_of_vals = n()) %>% 
+      filter(number_of_vals == n_of_samples) %>% 
+      ungroup() %>% 
+      subset(select = -c(`get(unit_to_impute)`, number_of_vals))
     # Reshape the data to impute
-    to_imput <- reshape2::dcast(dataset, 
+    to_imput <- reshape2::dcast(dataset,
                                 get(unit_to_impute) ~ sample_name, value.var= intensity_to_impute, fun.aggregate = median)
-    
     rownames(to_imput) <- to_imput[,1]
     to_imput <- to_imput[,-1]
-    
-    # Impute the data
     imputed_data <- complete(mice(to_imput, m = 5, method = "pmm", seed = 500))
-    
     # Reshape the data again to long format
     samples <- rep(colnames(imputed_data), each = length(unique(rownames(imputed_data))))
     pgs <- rep(rownames(imputed_data), times = length(unique(samples)))
     values <- unlist(as.vector(imputed_data), use.names = F)
     
-    imputed_data_long <- data_frame("sample_name" = samples,
-                                    unit_to_impute = pgs,
-                                    imputed_intensity = values)
+    imputed_data_long <- as.data.frame(tibble::tibble("sample_name" = samples,
+                                                      unit_to_impute = pgs,
+                                                      imputed_intensity = values))
     colnames(imputed_data_long)[2] <- unit_to_impute
-    #####dataset[,"normalized_intensity_before_imputation"] <- dataset[,intensity_to_impute]
-    #####    dataset <- subset(dataset, select = -c(normalized_intensity))
-    
     imputed_data <- merge(imputed_data_long, dataset, by = c("sample_name",unit_to_impute))
     imputed_data <- imputed_data %>%
       relocate(imputed_intensity, .after = all_of(intensity_to_impute))
-    
-    cols <- c(unit_to_impute)
-    imputed_data <- imputed_data %>%
-      group_by(across(all_of(cols))) %>%
-      filter(n() == n) %>%
-      ungroup()
-    if (report_results){
-    cat("tim - for NA imputation",file = "./results/used_parameters.txt",sep = "\n", append = T)
-    cat(paste0("impute == yes ",unique(NAs_prop)), file = "./results/used_parameters.txt", sep = "\n", append = T)
-    cat(paste0(rep("_",50), collapse = ""), file = "./results/used_parameters.txt",append = T, sep = "\n")}
+    dataset <- imputed_data
   }
-  if (presence_no_presence == T){
-    cat("Returning an expression matrix with all NAs to do presence/No presence analysis")
-    to_imput <- reshape2::dcast(dataset, 
-                                get(unit_to_impute) ~ sample_name, value.var= intensity_to_impute, fun.aggregate = median)
-    
-    rownames(to_imput) <- to_imput[,1]
-    to_imput <- to_imput[,-1]
-    expression_mat_presence_no_presence <<- to_imput
-    imputed_data <- "no data to be displayed as no imputation is done and this procedure is only done in order to not apply any statistics"
-  }
-  
-  return(imputed_data)
+  return(dataset)
 }
